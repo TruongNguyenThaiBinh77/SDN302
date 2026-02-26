c6040d519d87a12faa3055b72a0d9dac0355676e
TruongNguyenThaiBinh77 <binhtntse182370@fpt.edu.vn>
2026-02-26 14:45:49 +0700

    okoko

--- client/src/api/axiosConfig.js
+++ client/src/api/axiosConfig.js
@@ -1,21 +1,21 @@
-import axios from 'axios';
+import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'https://localhost:3443',
-    headers: {
-        'Content-Type': 'application/json',
-    },
+  baseURL: import.meta.env.VITE_API_URL || "https://localhost:3443",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 axiosInstance.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-        return config;
-    },
-    (error) => Promise.reject(error)
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error),
 );
 
 export default axiosInstance;
